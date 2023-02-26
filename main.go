136690f9346c5cf9e0af7f8baff50872010f16c0
shunkominato <shun19871001@gmail.com>
2023-02-26 10:29:00 +0900

    k

--- main.go
+++ main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
-
+	"github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/labstack/echo/v4"
 	// "github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
+	err := gorm.Open(
+		"postgres",
+		fmt.Sprintf(
+			"host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
+			"127.0.0.1", 5432, "postgres", "rails_sample", "postgres",
+		),
+	)
 	e := echo.New()
 
 	// e.Use(middleware.Logger())
